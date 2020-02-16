90e586d90927e56ae1a71d35c2b4d6786304b48b
Rafael <timbo.rafa@gmail.com>
2020-02-16 18:53:23 -0300

    deploy-api.sh verbosity

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -1,8 +1,11 @@
 CA_CLUSTER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ca-cluster)
 BR_CLUSTER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' br-cluster)
 
+echo "Starting cache_api"
 docker run -d --name cache_api \
     -p 5000:5000 \
     -e CA_CLUSTER_IP=$CA_CLUSTER_IP \
     -e BR_CLUSTER_IP=$BR_CLUSTER_IP \
-    timborafa/geo-cache-api 
+    timborafa/geo-cache-api
+
+echo "Success!"
