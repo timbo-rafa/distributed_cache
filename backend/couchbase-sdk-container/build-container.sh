90cda31a9d7cb8612ce3fc589276089444a67132
Rafael <timbo.rafa@gmail.com>
2020-02-16 21:15:18 -0300

    Dockerfile cleanup

--- backend/couchbase-sdk-container/build-container.sh
+++ backend/couchbase-sdk-container/build-container.sh
@@ -7,11 +7,8 @@ curl -L http://packages.couchbase.com/ubuntu/couchbase.key | apt-key add -
 echo "deb http://packages.couchbase.com/ubuntu bionic bionic/main" > /etc/apt/sources.list.d/couchbase.list
 
 apt-get update
-apt-get install -y libcouchbase-dbg libcouchbase-dev libcouchbase2* #python3-setuptools libev4 libev-dev libevent-dev libuv1-dev openssl libssl-dev
-
+apt-get install -y libcouchbase-dbg libcouchbase-dev libcouchbase2*
 ### Couchbase's Python SDK
 ##  from pip package
 pip3 install couchbase==2.5.10
 #pip3 install couchbase==3.0.0b3
-
-#apt-get autoremove -y
